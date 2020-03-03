b1d9020f6e63cb23fe54f303b946c638e0e6a1ec
Joe Brew <joebrew@gmail.com>
2020-03-03 18:20:04 +0100

    raw data indicators merge

--- data-raw/raw_data.R
+++ data-raw/raw_data.R
@@ -8,5 +8,23 @@ names(indicators) <- tolower(gsub(' ', '_', names(indicators)))
 usethis::use_data(indicators, overwrite = T)
 
 # Read in the full database
-db <- haven::read_dta('from_wb/hefpi_full_database.dta')
-usethis::use_data(db, overwrite = T)
+df <- haven::read_dta('from_wb/hefpi_full_database.dta')
+df$bin <- 
+  ifelse(df$level1 == 'fp', 'Financial Protection',
+         ifelse(df$level2 == 'h_cov', 'Healthcare Coverage',
+                ifelse(df$level2 == 'h_out', 'Health Outcomes', NA)))
+
+# Get nicer indicator names in full database
+df <- df %>%
+  left_join(right %>%
+              dplyr::select(variable_name:unit_of_measure),
+            by = c('indic' = 'variable_name'))
+usethis::use_data(df, overwrite = T)
+
+
+# # Read in the raw data from the website
+# library(tidyverse)
+# country <- read_csv('from_website/HEFPICountry.csv')
+# dat <- read_csv('from_website/HEFPIData.csv')
+# series <- read_csv('from_website/HEFPISeries.csv')
+
