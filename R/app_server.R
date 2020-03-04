af360d2d96ff8332557843e79c3dc6f0a00ced94
Joe Brew <joebrew@gmail.com>
2020-03-04 18:23:44 +0100

    examples for xing

--- R/app_server.R
+++ R/app_server.R
@@ -2,4 +2,35 @@
 app_server <- function(input, output,session) {
   # List the first level callModules here
   
+  output$plot1 <- renderPlot({
+    barplot(1:10, col = grey(seq(0, 1, length = 10)),
+            main = 'This is a plot',
+            sub = "You can't modify it from html/css, it's an image file")
+  })
+  
+  library(leaflet)
+  output$l1 <- leaflet::renderLeaflet({
+    leaflet() %>%
+      addProviderTiles('Esri.WorldImagery') %>%
+      addMarkers(data = tibble(lng = rnorm(10, sd = 40),
+                               lat = rnorm(10, sd = 40)))
+  })
+  
+  observeEvent(input$action, {
+    showModal(
+      modalDialog(title = 'This is a modal',
+                  size = 'l',
+                  easyClose = TRUE,
+                  fade = TRUE,
+                  fluidPage(
+                    fluidRow(
+                      column(6, h3('Here is some text'),
+                             p('And some sub-text')),
+                      column(6, h3('Here is some more text'),
+                             helpText('And some helper text'))
+                    )
+                  ))
+    )
+  })
+  
 }
