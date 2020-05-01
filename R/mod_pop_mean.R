75fe0a9eb705de451e3dd2ea921bf4cbd8ecc2e3
benmbrew <benmbrew@gmail.com>
2020-05-01 13:11:58 -0400

    issue with country input

--- R/mod_pop_mean.R
+++ R/mod_pop_mean.R
@@ -33,8 +33,7 @@ mod_pop_mean_ui <- function(id){
            selectInput(ns('region'), 'Region',
                        choices = region_list,
                        selected = region_list[[1]]),
-           selectInput(ns('country'), 'Country',
-                       choices = country_list))
+           uiOutput(ns('country_ui')))
   )
   
 }
@@ -50,68 +49,95 @@ mod_pop_mean_ui <- function(id){
 #' @keywords internal
 
 mod_pop_mean_server <- function(input, output, session){
+  # selected_country <- reactiveVal(value = NULL)
   
   # Observe changes to inputs in order to generate changes to the map
-  observeEvent({
-    input$interpolate
-    input$indicator
-    input$region
-    input$country
-    1
-  }, {
+  # observeEvent({
+  #   input$interpolate
+  #   input$indicator
+  #   input$region
+  #   input$country
+  #   1
+  # }, {
+    # message('event observed')
+    # the_country <- input$country
+    # selected_country(the_country)
     # Capture the interpolate input
-    interpolate <- input$interpolate
-    if(is.null(interpolate)){
-      interpolate <- 'Yes'
-    }
+    # interpolate <- input$interpolate
+    # if(is.null(interpolate)){
+    #   interpolate <- 'Yes'
+    # }
+    # 
+    # # Capture the indicator input
+    # indicator <- input$indicator
+    # if(is.null(indicator)){
+    #   indicator <- 'Change in poverty gap due to out-of-pocket health spending ($ 2011 PPP), $1.90 poverty line'
+    # }
+    # 
+    # 
+    # # Capture the region input
+    # region <- input$region
+    # if(is.null(region)){
+    #   region <- 'Latin America & Caribbean'
+    # }
     
-    # Capture the indicator input
-    indicator <- input$indicator
-    if(is.null(indicator)){
-      indicator <- 'Change in poverty gap due to out-of-pocket health spending ($ 2011 PPP), $1.90 poverty line'
-    }
-    
-    
-    # Capture the region input
-    region <- input$region
-    if(is.null(region)){
-      region <- 'Latin America & Caribbean'
-    }
-    
-    
-    # Capture the region input
-    country <- input$country
-    if(is.null(country)){
-      country <- c('China', 'Australia', 'Japan', 'Cambodia', 'Vietnam', 'Malaysia', 'New Zealand')
-    }
-    
-    
-    output$pop_mean <- renderPlot({
-      # Get the data to be plotted
-      df <- hefpi::df_series
-      pd <- df[df$region == region,]
-      # pd <- df[df$country_name %in% country,]
-      pd <- pd[pd$indicator_name == indicator,]
-      #remove countries with all NA in year columns
-      start_index <- which(names(pd) == 2001)
-      end_index <- which(names(pd) == 2015)
+  # })
+    # uioutput for country based on region
+    output$country_ui <- renderUI({
+      indicator <- input$indicator
+      region <- input$region
+      yn <- input$interpolate
+        
+        df <- hefpi::df_series
+        df <- df[df$indicator_name == indicator,]
+        df <- df[df$region == region,]
+        #remove countries with all NA in year columns
+        start_index <- which(names(df) == 2001)
+        end_index <- which(names(df) == 2015)
+        
+        good_index <- which(rowSums(is.na(df[, start_index:end_index])) != ncol(df[, start_index:end_index]))
+        df <- df[good_index,]
+        countries <- unique(df$country_name)
+        selectInput(inputId = 'country', 
+                    label = 'Country', 
+                    choices = countries,
+                    multiple = TRUE, selected = countries)
       
-      good_index <- which(rowSums(is.na(pd[, start_index:end_index])) != ncol(pd[, start_index:end_index]))
-      pd <- pd[good_index,]
-      
-      # put in long format for plotting
-      pd <- pd %>% select(country_name, indicator_name, `2001`:`2015`)
-      # pd[,3:ncol(pd)] <- apply(pd[, 3:ncol(pd)], 2, function(x) as.numeric(as.character(x)))
-      pd <- reshape2::melt(as.data.frame(pd), id.vars = c('country_name', 'indicator_name'))
-      pd$variable <- as.character(pd$variable)
-      save(pd, file = 'pd.rda')
-      p <- ggplot(pd, aes(variable, value, color = country_name)) + geom_point()
-      
-      return(p)
     })
     
-    
-  })
+  
+    output$pop_mean <- renderPlot({
+        indicator <- input$indicator
+        region <- input$region
+        yn <- input$interpolate
+        country_names <- input$country
+        message('the country_names are', country_names)
+        # save(country_names, file = 'cnames2.rda')
+        # Get the data to be plotted
+        df <- hefpi::df_series
+        pd <- df[df$indicator_name == indicator,]
+        pd <- pd[pd$region == region,]
+        pd <- pd[pd$country_name %in% country_names,]
+        #remove countries with all NA in year columns
+        start_index <- which(names(pd) == 2001)
+        end_index <- which(names(pd) == 2015)
+        
+        good_index <- which(rowSums(is.na(pd[, start_index:end_index])) != ncol(pd[, start_index:end_index]))
+        pd <- pd[good_index,]
+        
+        # put in long format for plotting
+        pd <- pd %>% select(country_name, indicator_name, `2001`:`2015`)
+        # pd[,3:ncol(pd)] <- apply(pd[, 3:ncol(pd)], 2, function(x) as.numeric(as.character(x)))
+        pd <- reshape2::melt(as.data.frame(pd), id.vars = c('country_name', 'indicator_name'))
+        pd$variable <- as.character(pd$variable)
+        # save(pd, file = 'pd.rda')
+        p <- ggplot(pd, aes(variable, value, color = country_name)) + geom_point()
+        
+        p
+      
+    })
+ 
+  
 }
 
 
