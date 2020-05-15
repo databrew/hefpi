d5f856edecbd7523000dcf51697285ba59697c57
benmbrew <benmbrew@gmail.com>
2020-05-15 16:33:35 -0400

    added x and y ranges and pop up box

--- R/mod_trends.R
+++ R/mod_trends.R
@@ -34,7 +34,21 @@ mod_trends_mean_ui <- function(id){
            selectInput(ns('region'), 'Region',
                        choices = as.character(region_list$region),
                        selected = 'East Asia & Pacific'),
-           uiOutput(ns('country_ui')))
+           uiOutput(ns('country_ui')),
+          sliderInput(ns('date_range'),
+                                  'Date range',
+                                  min = 1982,
+                                  max = 2017,
+                                  value = c(1982, 2017),
+                                  step = 1,
+                                  sep = ''),
+          sliderInput(ns('value_range'),
+                      'Y axis range',
+                      min = 0,
+                      max = 1,
+                      value = c(0, 1),
+                      step = 0.05,
+                      sep = ''))
   )
   )
 }
@@ -62,6 +76,7 @@ mod_trends_mean_server <- function(input, output, session){
       region <- input$region
       yn <- input$interpolate
       
+
       # get region code
       region_list <- hefpi::region_list
       region_code <- as.character(region_list$region_code[region_list$region == region])
@@ -75,12 +90,9 @@ mod_trends_mean_server <- function(input, output, session){
         df <- hefpi::df
         df <- df[df$indic == variable,]
         df <- df[df$regioncode == region_code,]
-        # For now, below is not needed, but will keep it in comments
-        # start_index <- which(names(df) == 2001)
-        # end_index <- which(names(df) == 2015)
-        # good_index <- which(rowSums(is.na(df[, start_index:end_index])) != ncol(df[, start_index:end_index]))
-        # df <- df[good_index,]
-        # create select input
+       
+       
+      
         countries <- unique(df$country)
         selectInput(session$ns("country"), 
                     label = 'Country', 
@@ -90,15 +102,19 @@ mod_trends_mean_server <- function(input, output, session){
     })
     
     output$trends_mean <- renderPlotly({
-      # indicator <- "Catastrophic health spending, 10%"
-      # region <- "Latin America & Caribbean"
-      # temp <- hefpi::df_series %>% filter(region == 'Latin America & Caribbean')
-      # country_names <- unique(temp$country_name)
+      indicator <- "Catastrophic health spending, 10%"
+      region <- "Latin America & Caribbean"
+      temp <- hefpi::df_series %>% filter(region == 'Latin America & Caribbean')
+      country_names <- unique(temp$country_name)
+      date_range <- c(1982, 2017)
+      value_range <- c(0,1)
       # get inputs
       indicator <- input$indicator
       region <- input$region
       yn <- input$interpolate
       country_names <- input$country
+      date_range <- input$date_range
+      value_range <- input$value_range
       # get region code 
       region_list <- hefpi::region_list
       indicators <- hefpi::indicators
@@ -120,6 +136,10 @@ mod_trends_mean_server <- function(input, output, session){
         df <- df[df$indic == variable,]
         df <- df[df$regioncode == region_code,]
         pd <- df[df$country %in% country_names,]
+        pd <- pd %>% filter(year >= min(date_range),
+                            year <= max(date_range)) 
+        pd <- pd %>% filter(pop >= min(value_range),
+                            pop <= max(value_range))
         
         # get title and subtitle
         plot_title <- paste0('Trend - population mean')
@@ -154,6 +174,174 @@ mod_trends_mean_server <- function(input, output, session){
       
       
     })
+}
+
+#-----------------------------------------------------------------------------------------------------
+#' @rdname mod_trends_con_ui
+#'
+#' @keywords internal
+#' @export 
+#' @import tidyverse
+#' @import ggplot2
+#' @import reshape2
+#' @importFrom shiny NS tagList 
+mod_trends_con_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+    
+    fluidPage(
+      column(8,
+             plotlyOutput(
+               ns('trends_con'),  height = '800px'
+             )),
+      column(4,
+             selectInput(ns('indicator'), 'Indicator',
+                         choices = indicators_list,
+                         selected = '4+ antenatal care visits'),
+             checkboxInput(ns('interpolate'), 'Interpolate missing values',
+                           value = TRUE),
+             selectInput(ns('region'), 'Region',
+                         choices = as.character(region_list$region),
+                         selected = 'East Asia and Pacific'),
+             uiOutput(ns('country_ui')),
+             sliderInput(ns('date_range'),
+                         'Date range',
+                         min = 1982,
+                         max = 2017,
+                         value = c(1982, 2017),
+                         step = 1,
+                         sep = ''),
+             sliderInput(ns('value_range'),
+                         'Y axis range',
+                         min = -1,
+                         max = 1,
+                         value = c(-1, 1),
+                         step = 0.05,
+                         sep = ''))
+    )
+  )
+}
+
+# Module Server
+#' @rdname mod_trends_cib_server
+#' @export
+#' @import tidyverse
+#' @import RColorBrewer
+#' @import ggplot2
+#' @import ggthemes
+#' @import scales
+#' @import reshape2
+#' @import htmltools
+#' @keywords internal
+
+mod_trends_con_server <- function(input, output, session){
+  output$country_ui <- renderUI({
+    
+ 
+    # get inputs
+    indicator <- input$indicator
+    region <- input$region
+    yn <- input$interpolate
+    country_names <- input$country
+    
+    # get region code
+    region_list <- hefpi::region_list
+    region_code <- as.character(region_list$region_code[region_list$region == region])
+    
+    # Get the variable
+    variable <- indicators %>%
+      filter(indicator_short_name == indicator) %>%
+      .$variable_name
+    
+    # subset data by variable and region code
+    df <- hefpi::df
+    df <- df[df$indic == variable,]
+    df <- df[df$regioncode == region_code,]
+    
+    
+    countries <- unique(df$country)
+    selectInput(session$ns("country"), 
+                label = 'Country', 
+                choices = countries,
+                multiple = TRUE, selected = countries)
+    
+  })
+  
+  output$trends_con <- renderPlotly({
+    # indicator <- "Catastrophic health spending, 10%"
+    # region <- "Latin America & Caribbean"
+    # temp <- hefpi::df_series %>% filter(region == 'Latin America & Caribbean')
+    # country_names <- unique(temp$country_name)
+    # date_range <- c(1982, 2017)
+    # value_range <- c(-1,1)
+    # get inputs
+    indicator <- input$indicator
+    region <- input$region
+    yn <- input$interpolate
+    country_names <- input$country
+    date_range <- input$date_range
+    value_range <- input$value_range
+    # get region code 
+    region_list <- hefpi::region_list
+    indicators <- hefpi::indicators
+    df <- hefpi::df
+    
+    if(is.null(country_names)){
+      NULL
+    } else {
+      
+      region_code <- as.character(region_list$region_code[region_list$region == region])
+      
+      # get variable
+      variable <- indicators %>%
+        filter(indicator_short_name == indicator) %>%
+        .$variable_name
+      
+      # subet by variable, region code and a list of countries
+      
+      df <- df[df$indic == variable,]
+      df <- df[df$regioncode == region_code,]
+      pd <- df[df$country %in% country_names,]
+      pd <- pd %>% filter(year >= min(date_range),
+                          year <= max(date_range)) 
+      pd <- pd %>% filter(CI >= min(value_range),
+                          CI <= max(value_range))
+      
+      # get title and subtitle
+      plot_title <- paste0('Trend - Concentration index')
+      sub_title <- indicator
+      
+      # text for plot
+      mytext <- paste(
+        "Country: ", as.character(pd$country),"\n", 
+        "Concentration Index: ", round(pd$CI, digits = 3), "\n",
+        "Year: ", as.character(pd$year),"\n",
+        "Data source: ", as.character(pd$referenceid_list), "\n",
+        sep="") %>%
+        lapply(htmltools::HTML)
+      # condition if we connect the dots
+      if(yn){
+        p <- plot_ly(data = pd, x = ~year, y = ~CI, color = ~country, 
+                     text = mytext, hoverinfo = 'text') %>%
+          add_trace(x = ~year, y = ~CI, color = ~country, mode = 'lines+markers') 
+        
+        
+      } else {
+        p <- plot_ly(data = pd, x = ~year, y = ~CI, color = ~country,
+                     text = mytext, hoverinfo = 'text')
+        
+      }
+      p <- p %>%
+        layout(title = plot_title,
+               xaxis= list(title = 'Year', showticklabels = TRUE),
+               yaxis= list(title = 'Value', showticklabels = TRUE))
+      p
+    }
+    
+    
+  })
+  
+  
 }
 
 #-----------------------------------------------------------------------------------------------------
@@ -294,154 +482,7 @@ mod_trends_quin_server <- function(input, output, session){
 }
 
 
-#-----------------------------------------------------------------------------------------------------
-#' @rdname mod_trends_con_ui
-#'
-#' @keywords internal
-#' @export 
-#' @import tidyverse
-#' @import ggplot2
-#' @import reshape2
-#' @importFrom shiny NS tagList 
-mod_trends_con_ui <- function(id){
-  ns <- NS(id)
-  tagList(
-    
-    fluidPage(
-      column(8,
-             plotlyOutput(
-               ns('trends_con'),  height = '800px'
-             )),
-      column(4,
-             selectInput(ns('indicator'), 'Indicator',
-                         choices = indicators_list,
-                         selected = '4+ antenatal care visits'),
-             checkboxInput(ns('interpolate'), 'Interpolate missing values',
-                           value = TRUE),
-             selectInput(ns('region'), 'Region',
-                         choices = as.character(region_list$region),
-                         selected = 'East Asia and Pacific'),
-             uiOutput(ns('country_ui')))
-    )
-  )
-}
 
-# Module Server
-#' @rdname mod_trends_cib_server
-#' @export
-#' @import tidyverse
-#' @import RColorBrewer
-#' @import ggplot2
-#' @import ggthemes
-#' @import scales
-#' @import reshape2
-#' @import htmltools
-#' @keywords internal
-
-mod_trends_con_server <- function(input, output, session){
-  output$country_ui <- renderUI({
-    
-
-    # get inputs
-    indicator <- input$indicator
-    region <- input$region
-    yn <- input$interpolate
-    
-    # get region code
-    region_list <- hefpi::region_list
-    region_code <- as.character(region_list$region_code[region_list$region == region])
-    
-    # Get the variable
-    variable <- indicators %>%
-      filter(indicator_short_name == indicator) %>%
-      .$variable_name
-    
-    # subset data by variable and region code
-    df <- hefpi::df
-    df <- df[df$indic == variable,]
-    df <- df[df$regioncode == region_code,]
-    # For now, below is not needed, but will keep it in comments
-    # start_index <- which(names(df) == 2001)
-    # end_index <- which(names(df) == 2015)
-    # good_index <- which(rowSums(is.na(df[, start_index:end_index])) != ncol(df[, start_index:end_index]))
-    # df <- df[good_index,]
-    # create select input
-    countries <- unique(df$country)
-    selectInput(session$ns("country"), 
-                label = 'Country', 
-                choices = countries,
-                multiple = TRUE, selected = countries)
-    
-  })
-  
-  output$trends_con <- renderPlotly({
-    # indicator <- "Catastrophic health spending, 10%"
-    # region <- "Europe & Central Asia"
-    # temp <- hefpi::df_series %>% filter(region == 'Europe & Central Asia')
-    # country_names <- unique(temp$country_name)
-    # get inputs
-    indicator <- input$indicator
-    region <- input$region
-    yn <- input$interpolate
-    country_names <- input$country
-    # get region code 
-    region_list <- hefpi::region_list
-    indicators <- hefpi::indicators
-    df <- hefpi::df
-    
-    if(is.null(country_names)){
-      NULL
-    } else {
-      
-      region_code <- as.character(region_list$region_code[region_list$region == region])
-      
-      # get variable
-      variable <- indicators %>%
-        filter(indicator_short_name == indicator) %>%
-        .$variable_name
-      
-      # subet by variable, region code and a list of countries
-      
-      df <- df[df$indic == variable,]
-      df <- df[df$regioncode == region_code,]
-      pd <- df[df$country %in% country_names,]
-      
-      # get title and subtitle
-      plot_title <- paste0('Trend - Concentration index')
-      sub_title <- indicator
-      
-      # text for plot
-      mytext <- paste(
-        "Country: ", as.character(pd$country),"\n", 
-        "Concentration Index: ", round(pd$CI, digits = 3), "\n",
-        "Year: ", as.character(pd$year),"\n",
-        "Data source: ", as.character(pd$referenceid_list), "\n",
-        sep="") %>%
-        lapply(htmltools::HTML)
-      # condition if we connect the dots
-      if(yn){
-        p <- plot_ly(data = pd, x = ~year, y = ~CI, color = ~country, 
-                     text = mytext, hoverinfo = 'text') %>%
-          add_trace(x = ~year, y = ~CI, color = ~country, mode = 'lines+markers') 
-        
-        
-      } else {
-        p <- plot_ly(data = pd, x = ~year, y = ~CI, color = ~country,
-                     text = mytext, hoverinfo = 'text')
-        
-      }
-      p <- p %>%
-        layout(title = plot_title,
-               xaxis= list(title = 'Year', showticklabels = TRUE),
-               yaxis= list(title = 'Value', showticklabels = TRUE))
-      p
-    }
-      
-    
-  })
-  
-  
-}
 ## To be copied in the UI
 # mod_trends_mean_ui("trends_mean1")
 # mod_trends_quin_ui("trends_quin1")
