f347b74e9eff5dedb51cad2708c404d1e07cc9b4
Joe Brew <joebrew@gmail.com>
2020-07-19 22:47:43 +0200

    minor change

--- R/mod_recent.R
+++ R/mod_recent.R
@@ -17,6 +17,8 @@
 #' @import shinyalert
 #' @importFrom shiny NS tagList 
 mod_recent_mean_ui <- function(id){
+  # let leaflet know that selections should persist
+  options(persistent = TRUE)
   ns <- NS(id)
   # tagList(
   fluidPage(
@@ -536,6 +538,12 @@ mod_recent_con_server <- function(input, output, session){
   
   
   output$recent_con_leaf <- renderLeaflet({
+    
+    # Plotly hover capture
+    mouse_event <- event_data("plotly_click", source = "subset")
+
+    print(mouse_event)
+    
     con_map <- get_con_map()
     if(is.null(con_map)){
       NULL
