62ad80b1cef2d883ba77e05c567c4b10a7805de0
benmbrew <benmbrew@gmail.com>
2021-03-03 09:01:36 -0500

    adding NS argument back to modules

--- R/mod_trends.R
+++ R/mod_trends.R
@@ -7,8 +7,8 @@
 #' @param output internal
 #' @param session internal
 #'
-#' @keywords internal
-#' @export 
+#' @importFrom shiny NS tagList 
+
 # UI FOR TRENDS (NATIONAL MEAN)
 mod_trends_mean_ui <- function(id){
   ns <- NS(id)
